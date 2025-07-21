b4e4b74ff99efb67270b353a42fd17d8f540de25
TakoSalakaia <takosalakaia0000@gmail.com>
2025-07-21 20:55:01 +0400

    first slide update

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    "pics/pic1.jpg",
+    "pics/pic2.jpg",
+    "pics/pic3.jpg",
+    "pics/pic4.jpg"
+  ];
+
+  let currentIndex = 0;
+  const sliderImage = document.getElementById("slider-image");
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    sliderImage.src = images[currentIndex];
+  }, 5000); // 5 
+});
